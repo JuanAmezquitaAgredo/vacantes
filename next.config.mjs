88cd95e503550c18c62a094655a9bf62e9445f8b
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-10-23 19:17:05 -0500

    add fix config.styled-component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    compiler: {
+      styledComponents: true,
+    },
+    reactStrictMode: true,
+  }
+  
+  export default nextConfig;
+  
