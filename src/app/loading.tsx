2ab9cb71bfbc6fd6537cb751ea0c247cee952543
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-10-29 10:59:18 -0500

    update loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-function loading() {
+const bounce = keyframes`
+  0%, 100% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1);
+  }
+`;
+
+const SpinnerWrapper = styled.div`
+  width: 100px;
+  height: 100px;
+  position: relative;
+  margin: 100px auto;
+`;
+
+const Bounce = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #a855f7;
+  opacity: 0.6;
+  position: absolute;
+  top: 0;
+  left: 0;
+  animation: ${bounce} 2s infinite ease-in-out;
+
+  &:nth-child(2) {
+    animation-delay: -1s;
+  }
+`;
+
+function Spinner() {
   return (
-    <div>
-      CARGANDO...
-    </div>
-  )
+    <SpinnerWrapper>
+      <Bounce />
+      <Bounce />
+    </SpinnerWrapper>
+  );
 }
 
-export default loading
+export default Spinner;
+
